625171ee7c70c9da7b225795578ad27c536df74f
Raghavendra Acharya <raghav@pratishthanventures.com>
2023-12-31 01:23:17 +0530

    env next config added

--- next.config.js
+++ next.config.js
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const webpack = require('webpack');
+const nextConfig = {
+    webpack: (cfg) => {
+        cfg.plugins.push(
+          new webpack.DefinePlugin({
+            'process.env.BACK_END_URL': JSON.stringify(process.env.BACK_END_URL),
+          })
+        );
+        return cfg;
+    },
+};
 
 module.exports = nextConfig;
